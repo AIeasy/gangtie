9329fa8ad36045594ac9815530c2cd8f20998915
Sean <zhongxinhu12138@gmail.com>
2022-08-12 17:49:04 -0400

    added login page,home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import Login from '../components/vue-Login.vue'
+import Home from '../components/vue-Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  { path: '/home', component: Home }
 ]
 
 const router = new VueRouter({
   routes
 })
+// 路由守卫
+/* eslint-disable  */
 
+router.beforeEach((to, from, next) => {
+  if(to.path === '/login') return next();
+  const mytoken = window.sessionStorage.getItem('token');
+  if (!mytoken) return next('/login');
+  next()
+})
+/* eslint-enable  */
 export default router
